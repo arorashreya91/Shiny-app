4467dfef409a91d5b1b3a3319215783469abfa75
arorashreya91 <arorashreya91@gmail.com>
2014-09-04 19:25:53 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -11,7 +11,15 @@ shinyUI(fluidPage(
     
     # Sidebar panel
     sidebarPanel(
-      sliderInput("size", "Area", 10, 1000, value = 100)
+      sliderInput("size", "Area", 10, 1000, value = 100),
+      div('The shiny app consists of the following:- 
+
+An area slider
+A graph between sepal width and sepal length of 3 species Iris setosa, versicolor, and virginica.
+As you increase or decrease the area with the slider you will the changes of sepal width and sepal length for different species.
+
+The iris dataset is a predefined data in R. It gives the measurements in centimeters of the variables sepal length and width and petal length and width, respectively, for 50 flowers from each of 3 species of iris. The species are Iris setosa, versicolor, and virginica.
+')
     ),
     
     # Main panel
